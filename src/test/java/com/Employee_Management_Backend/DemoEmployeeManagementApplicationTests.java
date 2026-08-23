15e95f8b48ce9cf38a82d68af896add0686934d3
shwetakamble020 <shwetakamble0201@gmail.com>
2026-08-23 20:52:55 +0530

    Initial commit - Employee Management System

---
+++ src/test/java/com/Employee_Management_Backend/DemoEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Employee_Management_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
